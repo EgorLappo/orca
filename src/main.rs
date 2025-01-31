1d5d5d73e51933522b919ccc552c5867d70248e6
Egor Lappo <elappo@stanford.edu>
2025-01-31 20:16:52 +0300

    fix simulation computation algorithm

--- src/main.rs
+++ src/main.rs
@@ -345,7 +345,7 @@ impl OrcaState for OrcaFullState {
                         kp * b
                             .iter()
                             .zip(markers.iter())
-                            .map(|(b, m)| b * m[i] + (1.0 - b) * (1.0 - m[i]))
+                            .map(|(b, &m)| b * m[i] + (1.0 - b) * (1.0 - m[i]))
                             .product::<f64>()
                     })
                     .fold(f64::NEG_INFINITY, f64::max)
@@ -382,9 +382,10 @@ impl Orca for OrcaSim {
 
 impl OrcaState for OrcaSimState {
     fn compute(&mut self, markers: Vec<&Vec<f64>>, k_prior: &[f64]) -> f64 {
+        // repeat `nsim` simulations
         (0..self.nsim)
             // generate the source population
-            // and the genotype
+            // and the genotype for each replicate
             .map(|_| {
                 let pop = sim_with_proportions(k_prior, &mut self.rng);
                 (
@@ -396,31 +397,54 @@ impl OrcaState for OrcaSimState {
                         .collect(),
                 )
             })
-            // map over all binary sequences, with b of length markers.len()
+            // try to assign the genotype to its own population...
             .map(|(pop, b): (_, Vec<_>)| {
-                // map over all populations
-                k_prior
-                    .iter()
-                    .enumerate()
-                    .map(|(i, kp)| {
-                        // in each population map over all markers
-                        kp * b
-                            .iter()
-                            .zip(markers.iter())
-                            .map(|(b, m)| b * m[i] + (1.0 - b) * (1.0 - m[i]))
-                            .product::<f64>()
-                    })
-                    // take argmax of this
-                    // and return 1 if it's the same as the source population
-                    .enumerate()
-                    .map(|(i, x)| if i == pop { (1, x) } else { (0, x) })
-                    // TODO: HOW TO CHOOSE RANDOMLY IN CASE OF TIE?
-                    .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-                    .unwrap()
-                    .0
+                // ... by computing gamma for each population, p.1187 of Rosenberg (2005)
+                let gammas = k_prior.iter().enumerate().map(|(i, kp)| {
+                    // in each population map over all markers
+                    kp * b
+                        .iter()
+                        .zip(markers.iter())
+                        .map(|(b, m)| b * m[i] + (1.0 - b) * (1.0 - m[i]))
+                        .product::<f64>()
+                });
+
+                // now two things must be done:
+                // first we must find max of gamma,
+                // then *count* how many max elements are there,
+                //    and whether any of these is the correct population
+                // if true, P(correct assignment) = 1/n_max,
+                //    otherwise, it's zero
+
+                // we will to this in one pass over `gammas`:
+                let (_, n_max, correct_is_maximal): (_, usize, bool) = gammas.enumerate().fold(
+                    (f64::NEG_INFINITY, 0, false),
+                    |(current, n, correct), (i, x)| {
+                        if x > current {
+                            // found new maximum, restart
+                            (x, 1, i == pop)
+                        } else if (x - current).abs() < f64::EPSILON {
+                            // found repeating maximum
+                            (x, n + 1, correct | (i == pop))
+                        } else {
+                            // not a maximum, proceed
+                            (current, n, correct)
+                        }
+                    },
+                );
+
+                // finally, decide on whether we managed to assign the population correctly
+                if !correct_is_maximal {
+                    // we failed, correct pop did not give maximal gamma
+                    0.0
+                } else {
+                    // otherwise we only fail if multiple populations with same max exist
+                    // then we randomly choose so our rate is
+                    1.0 / (n_max as f64)
+                }
             })
-            // get mean of the binary outcomes
-            .sum::<usize>() as f64
+            // get mean of the outcomes across all simulations
+            .sum::<f64>()
             / self.nsim as f64
     }
 }
@@ -515,7 +539,6 @@ fn cluster_proportions(data: &DataFrame, k: usize) -> Vec<f64> {
             .unwrap()
             .sum()
             .unwrap();
-        println!("computed for i {:?}", i);
         total += *f;
     }
 
